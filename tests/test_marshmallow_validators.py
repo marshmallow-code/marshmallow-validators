8ce93c01e2349bbf96b62fa6a45534cc2457e15f
Steven Loria <sloria1@gmail.com>
2019-08-18 11:35:00 -0400

    Fix tests (#20)

--- tests/test_marshmallow_validators.py
+++ tests/test_marshmallow_validators.py
@@ -18,27 +18,30 @@ class TestWTFormsValidation:
         field = fields.Field(validate=from_wtforms([AnyOf(["red", "blue"])]))
 
         assert field.deserialize("red") == "red"
-        with pytest.raises(MarshmallowValidationError) as excinfo:
+        with pytest.raises(MarshmallowValidationError, match="Invalid value"):
             field.deserialize("green")
-        assert "Invalid value" in str(excinfo)
 
     def test_from_wtforms_multi(self):
         field = fields.Field(
             validate=from_wtforms([Length(min=4), NoneOf(["nil", "null", "NULL"])])
         )
         assert field.deserialize("thisisfine") == "thisisfine"
-        with pytest.raises(MarshmallowValidationError) as excinfo:
+        with pytest.raises(
+            MarshmallowValidationError, match="Field must be at least 4 characters long"
+        ):
             field.deserialize("bad")
-        assert "Field must be at least 4 characters long" in str(excinfo)
-        with pytest.raises(MarshmallowValidationError) as excinfo:
+        with pytest.raises(
+            MarshmallowValidationError,
+            match="Invalid value, can't be any of: nil, null, NULL.",
+        ):
             field.deserialize("null")
-        assert "Invalid value, can't be any of: nil, null, NULL." in str(excinfo)
 
         with pytest.raises(MarshmallowValidationError) as excinfo:
             field.deserialize("nil")
         # both errors are returned
-        assert "Invalid value, can't be any of: nil, null, NULL." in str(excinfo)
-        assert "Field must be at least 4 characters long" in str(excinfo)
+        error = excinfo.value
+        assert "Invalid value, can't be any of: nil, null, NULL." in error.messages
+        assert "Field must be at least 4 characters long." in error.messages
 
     def test_from_wtforms_with_translation(self):
         field = fields.Field(
@@ -73,6 +76,8 @@ class TestColanderValidation:
     def test_from_colander(self):
         field = fields.Field(validate=from_colander([ContainsOnly([1])]))
         assert field.deserialize([1]) == [1]
-        with pytest.raises(MarshmallowValidationError) as excinfo:
+        with pytest.raises(
+            MarshmallowValidationError,
+            match="One or more of the choices you made was not acceptable",
+        ):
             field.deserialize([2])
-        assert "One or more of the choices you made was not acceptable" in str(excinfo)
